c8082debb57ee8418efe59006d89de5da9d2155c
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-06-19 23:11:14 +0200

    puppeteer instead of cheerio

--- scraping.js
+++ scraping.js
@@ -1,39 +1,30 @@
-// Importer les modules
-const axios = require("axios");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
-// Définir l'URL de la page à scraper
 const url =
 	"https://www.ldlc.com/informatique/pieces-informatique/carte-graphique-interne/c4684/+fv121-20576.html";
 
-// Créer une fonction asynchrone pour scraper les prix
-async function scrapePrices() {
+async function scrapeData() {
 	try {
-		// Faire la requête HTTP et obtenir le HTML
-		const response = await axios.get(url);
-		const html = response.data;
+		const browser = await puppeteer.launch({ headless: "new" });
 
-		// Charger le HTML avec cheerio
-		const $ = cheerio.load(html);
+		const page = await browser.newPage();
 
-		// Créer un tableau vide pour stocker les prix
-		const prices = [];
+		await page.goto(url);
 
-		// Sélectionner les éléments qui contiennent les prix avec un sélecteur CSS
-		$(".price").each((index, element) => {
-			// Extraire le texte de l'élément
-			const price = $(element).text();
-			// Ajouter le prix au tableau
-			prices.push(price);
-		});
+		const data = await page.$$eval(".listing-product .dsp-cell-right", (elements) =>
+			elements.map((el) => ({
+				title: el.querySelector(".pdt-desc a").textContent,
+				price: el.querySelector(".price").textContent,
+			}))
+		);
+		console.log(data);
 
-		// Afficher les résultats dans la console
-		console.log(prices);
+		await browser.close();
+
+		return data;
 	} catch (error) {
-		// Afficher l'erreur dans la console
 		console.error(error);
 	}
 }
 
-// Appeler la fonction pour scraper les prix
-scrapePrices();
+scrapeData();
